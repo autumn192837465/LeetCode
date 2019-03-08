661f57a4a3aba6c38e53f26837b4d2c0e6c8548a
autumn192837465 <autumn192837465@yahoo.com.tw>
2019-03-08 12:07:30 +0900

    Add files via upload

---
+++ 18. 4Sum.cpp
@@ -0,0 +1,90 @@
+/*
+	Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+
+	Note:
+
+	The solution set must not contain duplicate quadruplets.
+
+	Example:
+
+	Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+
+	A solution set is:
+	[
+	  [-1,  0, 0, 1],
+	  [-2, -1, 1, 2],
+	  [-2,  0, 0, 2]
+	]
+*/
+
+class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int>& nums, int target) {
+        vector<vector<int> > ans;
+        int n = nums.size();
+        if(n < 4)   return ans;
+
+        if(target >= 0){
+            sort(nums.begin(),nums.end());
+            for(int i=0;i<n-3;i++){
+                if(nums[i] > target) break;
+                if(i != 0 && nums[i]==nums[i-1])  continue;
+                for(int j=i+1;j<n-2;j++){
+                    if(nums[i] + nums[j] > target) break;
+                    if(j != i+1 && nums[j]==nums[j-1])  continue;
+                    int left = j+1;
+                    int right = n-1;
+                    while(right > left){
+                        int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        if(sum == target){
+                            ans.push_back(vector<int> {nums[i],nums[j],nums[left],nums[right]});
+
+                            while(left < right && nums[left] == nums[left+1])   left++;
+                            while(left < right && nums[right] == nums[right-1])   right--;
+                            left++;
+                            right--;
+                        }
+                        else if(sum > target){
+                            right--;
+                        }
+                        else{
+                            left++;
+                        }
+                    }
+                }
+            }
+        }
+        else{
+            sort(nums.begin(),nums.end(),greater<int>());
+            for(int i=0;i<n;i++)    cout<<nums[i]<<" ";
+            for(int i=0;i<n-3;i++){
+                if(nums[i] < target) break;
+                if(i != 0 && nums[i]==nums[i-1])  continue;
+                for(int j=i+1;j<n-2;j++){
+                    if(nums[i] + nums[j] < target) break;
+                    if(j != i+1 && nums[j]==nums[j-1])  continue;
+                    int left = j+1;
+                    int right = n-1;
+                    while(right > left){
+                        int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                        if(sum == target){
+                            ans.push_back(vector<int> {nums[i],nums[j],nums[left],nums[right]});
+
+                            while(left < right && nums[left] == nums[left+1])   left++;
+                            while(left < right && nums[right] == nums[right-1])   right--;
+                            left++;
+                            right--;
+                        }
+                        else if(sum > target){
+                            left++;
+                        }
+                        else{
+                            right--;
+                        }
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};	
