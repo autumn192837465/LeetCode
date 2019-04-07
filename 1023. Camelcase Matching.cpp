c6a48d4c446d547879286df3aaef5d7a238abf88
Scott <autumn192837465@yahoo.com.tw>
2019-04-08 00:54:57 +0900

    Add 1023

---
+++ 1023. Camelcase Matching.cpp
@@ -0,0 +1,69 @@
+/*
+	A query word matches a given pattern if we can insert lowercase letters to the pattern word so that it equals the query. (We may insert each character at any position, and may insert 0 characters.)
+
+	Given a list of queries, and a pattern, return an answer list of booleans, where answer[i] is true if and only if queries[i] matches the pattern.
+
+	 
+
+	Example 1:
+
+	Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FB"
+	Output: [true,false,true,true,false]
+	Explanation: 
+	"FooBar" can be generated like this "F" + "oo" + "B" + "ar".
+	"FootBall" can be generated like this "F" + "oot" + "B" + "all".
+	"FrameBuffer" can be generated like this "F" + "rame" + "B" + "uffer".
+	Example 2:
+
+	Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBa"
+	Output: [true,false,true,false,false]
+	Explanation: 
+	"FooBar" can be generated like this "Fo" + "o" + "Ba" + "r".
+	"FootBall" can be generated like this "Fo" + "ot" + "Ba" + "ll".
+	Example 3:
+
+	Input: queries = ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"], pattern = "FoBaT"
+	Output: [false,true,false,false,false]
+	Explanation: 
+	"FooBarTest" can be generated like this "Fo" + "o" + "Ba" + "r" + "T" + "est".
+	 
+
+	Note:
+
+	1 <= queries.length <= 100
+	1 <= queries[i].length <= 100
+	1 <= pattern.length <= 100
+	All strings consists only of lower and upper case English letters.
+*/
+class Solution {
+public:
+    vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        vector<bool> ans(queries.size(),false);
+
+        for(int i = 0;i<queries.size();i++){
+            int patIdx = 0;
+            int queIdx = 0;
+
+            while(queIdx < queries[i].size() && patIdx < pattern.size()){
+                if(queries[i][queIdx] == pattern[patIdx]){
+                    patIdx ++;
+                }
+                else if(queries[i][queIdx] <= 'Z' && queries[i][queIdx] >= 'A'){
+                    queIdx = queries[i].size();
+                    break;
+                }
+                queIdx++;
+            }
+            while(queIdx < queries[i].size()){
+                if(queries[i][queIdx] <= 'Z' && queries[i][queIdx] >= 'A'){
+                    ans[i] = false;
+                    break;
+                }
+                queIdx ++;
+            }
+            ans[i] = (patIdx == pattern.size() && queIdx == queries[i].size());
+        }
+
+        return ans;
+    }
+};
