b1fdd65e7daeb81707d8421cc97d354ec8c676a5
Scott <autumn192837465@yahoo.com.tw>
2019-05-11 17:21:16 +0900

    Add 342

---
+++ 342. Power of Four.cpp
@@ -0,0 +1,25 @@
+/*
+	Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+	Example 1:
+
+	Input: 16
+	Output: true
+	Example 2:
+
+	Input: 5
+	Output: false
+	Follow up: Could you solve it without loops/recursion?
+*/
+
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        if(num == 0)    return false;
+        if(num == 1)    return true;
+        while(num%4==0){
+            num/=4;
+        }
+        return (num==1)?true:false;
+    }
+};
