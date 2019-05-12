63db5683f66632e0e7a7915e0abc8a6bd2270e26
Scott <autumn192837465@yahoo.com.tw>
2019-05-12 17:48:56 +0900

    Add 357

---
+++ 357. Count Numbers with Unique Digits.cpp
@@ -0,0 +1,27 @@
+/*
+	Given a non-negative integer n, count all numbers with unique digits, x, where 0 ≤ x < 10n.
+
+	Example:
+
+	Input: 2
+	Output: 91 
+	Explanation: The answer should be the total numbers in the range of 0 ≤ x < 100, 
+				 excluding 11,22,33,44,55,66,77,88,99
+*/
+
+class Solution {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        if(n == 0)  return 1;
+        if(n == 1)  return 10;
+        int ans = 10;
+        for(int i = 2;i<=n;i++){
+            int temp = 9;
+            for(int j=1;j<i;j++){
+                temp *= (10-j);
+            }
+            ans += temp;
+        }
+        return ans;
+    }
+};
