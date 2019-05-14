78a81dcd4d2af2af82b22c0b0ead83c6843d562b
Scott <autumn192837465@yahoo.com.tw>
2019-05-14 22:39:18 +0900

    Add 299

---
+++ 299. Bulls and Cows.cpp
@@ -0,0 +1,62 @@
+/*
+	You are playing the following Bulls and Cows game with your friend: You write down a number and ask your friend to guess what the number is. Each time your friend makes a guess, you provide a hint that indicates how many digits in said guess match your secret number exactly in both digit and position (called "bulls") and how many digits match the secret number but locate in the wrong position (called "cows"). Your friend will use successive guesses and hints to eventually derive the secret number.
+
+	Write a function to return a hint according to the secret number and friend's guess, use A to indicate the bulls and B to indicate the cows. 
+
+	Please note that both secret number and friend's guess may contain duplicate digits.
+
+	Example 1:
+
+	Input: secret = "1807", guess = "7810"
+
+	Output: "1A3B"
+
+	Explanation: 1 bull and 3 cows. The bull is 8, the cows are 0, 1 and 7.
+	Example 2:
+
+	Input: secret = "1123", guess = "0111"
+
+	Output: "1A1B"
+
+	Explanation: The 1st 1 in friend's guess is a bull, the 2nd or 3rd 1 is a cow.
+	Note: You may assume that the secret number and your friend's guess only contain digits, and their lengths are always equal.
+*/
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        // Find A
+        int A = 0;
+        int B = 0;
+
+        bool usedA[secret.size()];
+        bool usedB[secret.size()];
+        for(int i =0;i<secret.size();i++){
+            usedA[i] = false;
+            usedB[i] = false;
+        }
+
+        for(int i =0;i<secret.size();i++){
+            if(secret[i] == guess[i]){
+                A++;
+                usedA[i] = true;
+                usedB[i] = true;
+            }
+         }
+        // Find B
+        for(int i = 0;i<secret.size();i++){
+            if(usedA[i]) continue;
+            for(int j = 0;j<secret.size();j++){
+                if(i == j)  continue;
+                else if(usedB[j]) continue;
+                if(secret[i] == guess[j]){
+                    B++;
+                    usedB[j] = true;
+                    break;
+                }
+            }
+        }
+
+        return to_string(A) + "A" + to_string(B) + "B";
+    }
+};
