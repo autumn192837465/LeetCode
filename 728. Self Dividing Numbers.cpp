e0c3e97ad81065a94d04a9024569dc0e4f49b1a2
Scott <autumn192837465@yahoo.com.tw>
2019-12-28 21:08:14 +0800

    Add 728

---
+++ 728. Self Dividing Numbers.cpp
@@ -0,0 +1,41 @@
+/*
+	A self-dividing number is a number that is divisible by every digit it contains.
+
+	For example, 128 is a self-dividing number because 128 % 1 == 0, 128 % 2 == 0, and 128 % 8 == 0.
+
+	Also, a self-dividing number is not allowed to contain the digit zero.
+
+	Given a lower and upper number bound, output a list of every possible self dividing number, including the bounds if possible.
+
+	Example 1:
+	Input: 
+	left = 1, right = 22
+	Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+	Note:
+
+	The boundaries of each input argument are 1 <= left <= right <= 10000.
+*/
+
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for(int i = left;i<=right;i++){
+            if(CheckVaild(i)){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+    bool CheckVaild(int &num){
+        int temp = num;
+        while(temp){
+            int divisor = temp%10;
+            if(divisor == 0)    return false;
+            if(num % divisor != 0)  return false;
+            temp /=10;
+        }
+        return true;
+    }
+};
+
